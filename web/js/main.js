2729c5174ae6306f3dd1954688d2d3b39038bfa5
Germán Dubienczuk <dubienczukg@gmail.com>
2026-02-10 12:04:55 -0300

    f

--- web/js/main.js
+++
@@ -1,9 +0,0 @@
-// inicializar modulos
-import {initMenu} from './menu.js';
-import {initScrollReveal} from './scroll.js';
-import {initHover} from './hover.js';
-document.addEventListener('DOMContentLoaded',()=>{
-  initMenu();
-  initScrollReveal();
-  initHover();
-});
--- web/js/menu.js
+++
@@ -1,9 +0,0 @@
-// inicializar menu movil
-export function initMenu(){
-  const toggle=document.querySelector('.nav__toggle');
-  const list=document.querySelector('.nav__list');
-  toggle.addEventListener('click',()=>list.classList.toggle('nav__list--open'));
-  list.querySelectorAll('a').forEach(link=>
-    link.addEventListener('click',()=>list.classList.remove('nav__list--open'))
-  );
-}
